5546b6cd3025cc5288ce5f63408634f8804ec0cd
Baran Çakı <baran.caki@hotmail.com>
2025-08-12 14:39:33 +0300

    update

--- Scrape/deustche_messe_sablon.py
+++ Scrape/deustche_messe_sablon.py
@@ -14,6 +14,7 @@ import re
 import urllib.parse
 from webdriver_manager.chrome import ChromeDriverManager
 from table_to_sql import save_to_sqlite,create_mysql_dump_from_sqlite
+from Scrape.scrape import bing_ilk_link_al,site_icerisinden_email_bul
 
 def scrape_win_eurasia_all_pages(url, sayfa_sayisi):
     options = Options()
--- Scrape/scrape.py
+++ Scrape/scrape.py
@@ -15,6 +15,25 @@ import urllib.parse
 from webdriver_manager.chrome import ChromeDriverManager
 from table_to_sql import save_to_sqlite,create_mysql_dump_from_sqlite
 
+def kutuphane():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    import pandas as pd
+    import streamlit as st
+    import time
+    import plotly.express as px
+    import requests
+    from bs4 import BeautifulSoup
+    import re
+    import urllib.parse
+    from webdriver_manager.chrome import ChromeDriverManager
+    from table_to_sql import save_to_sqlite,create_mysql_dump_from_sqlite
+    from Scrape.scrape import bing_ilk_link_al,site_icerisinden_email_bul
+
 def google_ilk_link_manual(firma_adi):
     query = urllib.parse.quote(firma_adi)
     url = f"https://www.google.com/search?q={query}"
